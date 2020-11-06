09ec709d53926be12c5ccf49b767915bc8045a8c
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-06 11:52:55 +0900

    Feat : nodemon 설정
    nodemon을 이용한 개발 환경 설정을 진행했습니다.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
 var indexRouter = require("./routes/index");
+require("dotenv").config();
 
 var app = express();
 
