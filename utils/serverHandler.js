c2bf2e96e0434fff02c154c3aa771987d16c5380
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-11-06 14:32:25 +0900

    Feat : health check를 통한 서버 제외처리
    health check를 통해서 장애가 있는 서버에는 요청을 제외하도록 구현했습니다.

--- utils/serverHandler.js
+++ utils/serverHandler.js
@@ -2,12 +2,15 @@ var request = require("request");
 
 const serverList = [
   "http://localhost:5000",
-  "http://이슈잇슈.홈페이지.한국:5000",
+  "http://localhost:500",
+  "http://localhost:50",
 ];
 
 let cur = 0;
+let errServer = new Array();
 
-exports.logMiddleware = (req, res, next) => {
+// req, res logging
+const logMiddleware = (req, res, next) => {
   const start = Date.now();
   console.log("Start", serverList[cur], "서버 번호" + cur);
   res.on("finish", () => {
@@ -17,13 +20,41 @@ exports.logMiddleware = (req, res, next) => {
   next();
 };
 
-exports.serverHandler = (req, res) => {
+// 서버 분산 요청
+const serverHandler = (req, res) => {
   const serverReq = request({ url: serverList[cur] + req.url }).on(
     "error",
     (error) => {
-      return res.status(500).json(error.message);
+      healthCheck(req, res);
     }
   );
   req.pipe(serverReq).pipe(res);
   cur = (cur + 1) % serverList.length;
 };
+
+// server health check
+function healthCheck(req, res) {
+  let serverNum = prevServer(cur);
+
+  const err = serverList.splice(serverNum, 1);
+  errServer.push(err[0]);
+  cur = prevServer(serverNum);
+  console.log("에러", errServer, serverList);
+
+  if (serverList.length === 0) {
+    console.log("[모든 서버 장애] 대응가능한 서버 없음", serverList[serverNum]);
+    return res.status(500).json(error.message);
+  }
+  serverHandler(req, res);
+}
+
+// 이전 서버url 인덱스 번호
+function prevServer(cur) {
+  let serverNum = cur - 1;
+  if (serverNum < 0) {
+    serverNum = serverList.length - 1;
+  }
+  return serverNum;
+}
+
+module.exports = { serverHandler, logMiddleware };
